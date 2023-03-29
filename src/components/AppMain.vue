8febd407d10b2d4f3fc99a8897feed03e3cf7fe0
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-03-29 15:39:26 +0200

    COMPLETATA MILESTONE 4

---
+++ src/components/AppMain.vue
@@ -0,0 +1,50 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: "AppMain",
+
+    data() {
+
+        return {
+
+            apiUrlAllProjects: 'http://127.0.0.1:8000/api/projects',
+
+            projects: [],
+
+        }
+    },
+
+    methods: {
+
+        getProjects() {
+
+            axios
+                .get(this.apiUrlAllProjects)
+                .then(response => {
+
+                    this.projects = response.data.projects;
+
+                    console.log(this.projects);
+
+                })
+
+        }
+
+    },
+
+    created() {
+
+        this.getProjects();
+
+    }
+};
+</script>
+
+<template>
+  
+
+</template>
+
+<style scoped>
+</style>
