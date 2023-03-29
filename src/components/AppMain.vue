56af8ce62a85c728d7f83b8fd1096b681a4bea78
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-03-29 17:05:28 +0200

    BONUS COMPLETATO

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -17,6 +17,10 @@ export default {
 
             projects: [],
 
+            currentPage: 1,
+
+            lastPage: 1
+
         }
     },
 
@@ -25,14 +29,30 @@ export default {
         getProjects() {
 
             axios
-                .get(this.apiUrlAllProjects)
+                .get(this.apiUrlAllProjects, {
+                    params: {
+                        page: this.currentPage
+                    }
+                })
                 .then(response => {
 
-                    this.projects = response.data.projects;
+                    this.projects = response.data.projects.data;
 
                     console.log(this.projects);
 
+                    this.lastPage = response.data.projects.last_page
+
+                    console.log(this.currentPage);
+
                 })
+
+        },
+
+        changePage(page) {
+
+            this.currentPage = page;
+
+            this.getProjects();
 
         }
 
@@ -52,11 +72,41 @@ export default {
   
     <AppProjectCard :projects="projects"/>
 
+    <div class="container-btn">
+
+        <div class="btn">
+
+            <button @click="changePage(num)" v-for="num in lastPage">{{ num }}</button>
+
+        </div>
+
+    </div>
+
 </template>
 
 <style lang="scss" scoped>
 h1 {
     padding: 20px;
     color: #3B5998;
+}
+.container-btn {
+    padding: 40px 0;
+    display: flex;
+    justify-content: center;
+    .btn {
+        button {
+            display: inline-block;
+            margin: 0 10px;
+            padding: 15px;
+            font-weight: bold;
+            color: #3B5998;
+            cursor: pointer;
+            border-top: none;
+            border-left: none;
+            border-right: none;
+            background-color: #EDEDED;
+            border-bottom: 2px solid #3B5998;
+        }
+    }
 }
 </style>
